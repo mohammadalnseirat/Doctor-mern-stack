f54a78724372aa50a84635d63f33d72c8285839f
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-10-16 16:24:08 +0400

    Create Pages UI And Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import MyProfile from "./pages/MyProfile";
+import Doctors from "./pages/Doctors";
+import MyAppointments from "./pages/MyAppointments";
+import Appointment from "./pages/Appointment";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:speciality" element={<Doctors />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/appointment/:doctorId" element={<Appointment />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
